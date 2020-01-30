fa1dc61415c3524a37cf7f580c28dc3327527170
aureliano sinatra <aureliano.sinatra@eglobalmark.com>
2020-01-30 12:08:50 +0100

    changed script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 value=$(<payload.json)
-mqtt-bench publish --host 192.168.1.207 --port 1883 --topic "latency" --qos 0 --thread-num 10 --publish-num 1 --message $value
+mqtt-bench publish --host 192.168.1.207 --port 1883 --topic "latency" --qos 0 --thread-num 10 --publish-num 1 --message "$(value)"
